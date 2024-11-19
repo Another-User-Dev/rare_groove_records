484253ccea2d30f50998ad7bc594bfd3dd53ae09
Michael Mohamed <michael.jobsearch.emails@gmail.com>
2024-11-19 14:48:48 +0000

    add home app and templates

--- rare_groove_records/settings.py
+++ rare_groove_records/settings.py
@@ -42,8 +42,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-
-
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -61,7 +60,10 @@ ROOT_URLCONF = 'rare_groove_records.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+            os.path.join(BASE_DIR, 'templates', 'allauth'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
