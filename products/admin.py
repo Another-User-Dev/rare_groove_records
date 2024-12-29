8d1728cb11940e32b17792ae5bf873f2d93e8d95
Michael Mohamed <michael.jobsearch.emails@gmail.com>
2024-12-29 00:37:00 +0000

    Customise admin

--- products/admin.py
+++ products/admin.py
@@ -2,8 +2,46 @@ from django.contrib import admin
 from .models import Product, Category, Genre, Rating, Format
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
-admin.site.register(Genre)
-admin.site.register(Rating)
-admin.site.register(Format)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'category',
+        'artist',
+        'title',
+        'description',
+        'genre',
+        'format',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = (
+        'genre',
+    )
+
+class RatingAdmin(admin.ModelAdmin):
+    list_display = (
+        'rating',
+    )
+
+class FormatAdmin(admin.ModelAdmin):
+    list_display = (
+        'format',
+    )
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(Rating, RatingAdmin)
+admin.site.register(Format, FormatAdmin)
