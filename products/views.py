3d63426905acac92ed4c83ff9069b79ee54fe576
Michael Mohamed <michael.jobsearch.emails@gmail.com>
2024-12-30 17:01:56 +0000

    Add sorting functionality for products

--- products/views.py
+++ products/views.py
@@ -13,8 +13,23 @@ def all_products(request):
     categories = None
     genres = None
     formats = None
-
+    sort = None
+    direction = None
+    
     if request.GET:
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
         if 'format' in request.GET:
             formats = request.GET['format'].split(',')
             products = products.filter(format__format__in=formats)
@@ -39,12 +54,15 @@ def all_products(request):
             queries = Q(title__icontains=query) | Q(artist__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
         'current_genres': genres,
         'current_formats': formats,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
