f0af973f1396ec98ee0dbeac8751cd048e81c5fe
Michael Mohamed <michael.jobsearch.emails@gmail.com>
2025-01-19 15:02:13 +0000

    Deployment second part

--- rare_groove_records/settings.py
+++ rare_groove_records/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
+import dj_database_url
 
 if os.path.exists("env.py"):
   import env 
@@ -29,7 +30,7 @@ SECRET_KEY = 'django-insecure-03)onvnxgqg*z7q3&s)!ud5hk_*uxy*(e_@z7j+i=t6e4sp@%g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-anotheruser-raregroover-elhyqz6bhfi.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['rare-groove-records.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -110,7 +111,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
-ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
@@ -122,13 +123,17 @@ WSGI_APPLICATION = 'rare_groove_records.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
-
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
